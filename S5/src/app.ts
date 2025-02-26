99735b30e4b927c84e023e4ab9331dad69a68b64
[Projet] <[r.marcelli31@gmail.com]>
2025-02-26 23:21:27 +0100

    EP2

--- S5/src/app.ts
+++ S5/src/app.ts
@@ -4,3 +4,13 @@ import { creerScore, afficherScore } from "./tuples.js";
 
 const score = creerScore();
 afficherScore(score);
+
+
+// EP2 
+
+import { Direction, vecteurDirection } from "./enums.js";
+
+console.log("Vecteur Nord :", vecteurDirection(Direction.Nord));
+console.log("Vecteur Sud :", vecteurDirection(Direction.Sud));
+console.log("Vecteur Est :", vecteurDirection(Direction.Est));
+console.log("Vecteur Ouest :", vecteurDirection(Direction.Ouest));
