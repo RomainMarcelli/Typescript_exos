0ba54d2ce97e2aa3abdad0fcdba9ecf08d03a619
[Projet] <[r.marcelli31@gmail.com]>
2025-02-26 22:41:22 +0100

    EP2

--- S3/src/app.ts
+++ S3/src/app.ts
@@ -7,3 +7,14 @@ console.log("Longueur de la chaîne :", resultat1);
 
 const resultat2 = traiterValeur(5);
 console.log("Double du nombre :", resultat2); 
+
+
+// EP2
+
+import { Rectangle, Cercle, afficherSurface } from "./formes.js";
+
+const monRectangle = new Rectangle(12, 12);
+const monCercle = new Cercle(8);
+
+afficherSurface(monRectangle); 
+afficherSurface(monCercle); 
