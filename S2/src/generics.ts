a56e56cf1d094c5e3e6bede854d1d3da5bbf7d82
[Projet] <[r.marcelli31@gmail.com]>
2025-02-26 22:14:48 +0100

    EP3

--- S2/src/generics.ts
+++ S2/src/generics.ts
@@ -2,6 +2,7 @@ export function premierElement<T>(tableau: T[]): T | undefined {
     return tableau.length > 0 ? tableau[0] : undefined;
 }
 
+// EP3
 export function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
     return obj[key];
 }
@@ -12,7 +13,6 @@ export interface Voiture {
     annee: number;
 }
 
-// 3 - Création d'un objet respectant l'interface Voiture
 export const maVoiture: Voiture = {
     marque: "Toyota",
     modele: "Corolla",
