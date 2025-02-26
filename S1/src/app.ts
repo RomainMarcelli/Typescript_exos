6f8b866feaf3d529b70d64f07f1067a2687db47d
[Projet] <[r.marcelli31@gmail.com]>
2025-02-26 12:21:55 +0100

    EP3 fini

--- S1/src/app.ts
+++ S1/src/app.ts
@@ -5,3 +5,21 @@ afficherPersonne(personne1);
 // EP2 : 3. TEST
 afficherPersonneAvancee(personne2);
 afficherPersonneAvancee(personne3);
+
+// EP3 
+
+import { PersonnePro, afficherInfoProfessionnelle } from "./personnePro.js";
+
+const employe1: PersonnePro = {
+    nom: "Alice",
+    poste: "Développeuse"
+};
+
+const client1: PersonnePro = {
+    nom: "Bob",
+    entreprise: "TechCorp"
+};
+
+
+afficherInfoProfessionnelle(employe1); // Employé: Alice travaille comme Développeuse.
+afficherInfoProfessionnelle(client1);
---
+++ S1/src/personnePro.ts
@@ -0,0 +1,25 @@
+// EP3 : 1 - Création de deux interfaces :
+
+export interface Employe {
+    nom: string;
+    poste: string;
+}
+
+export interface Client {
+    nom: string;
+    entreprise: string;
+}
+
+// EP3: 2 - Définition d’un type union :
+
+export type PersonnePro = Employe | Client;
+
+
+// EP3: 3 - Fonction avec Narrowing
+export function afficherInfoProfessionnelle(personne: PersonnePro): void {
+    if ("poste" in personne) {
+        console.log(`Employé: ${personne.nom} travaille comme ${personne.poste}.`);
+    } else if ("entreprise" in personne) {
+        console.log(`Client: ${personne.nom} est associé à l'entreprise ${personne.entreprise}.`);
+    }
+}
