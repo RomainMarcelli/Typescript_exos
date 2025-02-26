faa6e4c526700579f737c279dcad0e1a2dbf8ab5
[Projet] <[r.marcelli31@gmail.com]>
2025-02-26 12:16:45 +0100

    EP2 fini

--- S1/src/app.ts
+++ S1/src/app.ts
@@ -1,3 +1,7 @@
-import { afficherPersonne, personne1} from "./personne.js";
+import { afficherPersonne, personne1, afficherPersonneAvancee, personne2, personne3} from "./personne.js";
 
 afficherPersonne(personne1);
+
+// EP2 : 3. TEST
+afficherPersonneAvancee(personne2);
+afficherPersonneAvancee(personne3);
--- S1/src/personne.ts
+++ S1/src/personne.ts
@@ -1,13 +1,52 @@
-// 2 - Définition de l'interface :
-export interface Personne {
-    name: string;
-    age: number;
+// EP2 : 1. Extension de l'interface Personne
+
+export interface Adresse {
+    rue: string;
+    ville: string;
 }
 
-// 3 - Implémentation d'une fonction d'affichage :
+// EP1 : 2 - Définition de l'interface :
+export interface Personne {
+    readonly id: number;
+    name: string;
+    age: number;
+    // // EP2 : 1. Extension de l'interface Personne
+    adresse?: Adresse;  // Propriété optionnelle
+}
+
+// EP1 : 3 - Implémentation d'une fonction d'affichage :
 export function afficherPersonne(personne: Personne) {
     console.log(`Bonjour, je m'appelle ${personne.name} et j'ai ${personne.age} ans.`);
 }
 
-// 4 - Création d'un objet et appel de la fonction :
-export const personne1: Personne = { name: "Alice", age: 25 };
+// EP1 : 4 - Création d'un objet et appel de la fonction :
+export const personne1: Personne = { id: 1, name: "Alice", age: 25 };
+
+
+// EP2 : 2 - Fonction d'affichage avancée :
+export function afficherPersonneAvancee(personne: Personne) {
+    if (personne.adresse) {
+        console.log(`Bonjour, je m'appelle ${personne.name} (ID: ${personne.id}), j'ai ${personne.age} ans et j'habite au ${personne.adresse.rue}, ${personne.adresse.ville}.`);
+    } else {
+        console.log(`Bonjour, je m'appelle ${personne.name} (ID: ${personne.id}) et j'ai ${personne.age} ans.`);
+    }
+}
+
+// EP2 :3 - Test 
+export const personne2: Personne = { 
+    id: 2,
+    name: "Dilan", 
+    age: 25,
+    adresse: { 
+        rue: "10 rue des Lilas", 
+        ville: "Paris" 
+    }
+};
+
+// EP2 :3 - Test 
+export const personne3: Personne = { 
+    id: 3,
+    name: "Bob", 
+    age: 30
+};
+
